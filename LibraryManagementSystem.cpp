8bf865f5df2d33a881bacb79baa9f9d5eb0d99ea
RushikeshPachpute <rushikeshpachpute13@gmail.com>
2023-12-29 18:41:49 +0530

    LibraryManagementSystem Project added1

--- LibraryManagementSystem.cpp
+++ LibraryManagementSystem.cpp
@@ -9,7 +9,7 @@ private:
     bool available;
 
 public:
-  Book(string sTitle, string sAuthor, string sISBN, bool sAvailable)  //Parameterizeed Constructor
+    Book(string sTitle, string sAuthor, string sISBN, bool sAvailable) // Parameterizeed Constructor
     {
         title = sTitle;
         author = sAuthor;
@@ -32,7 +32,7 @@ public:
     // {
     //     available = sAvailable;
     // }
-  
+
     string getTitle()
     {
         return title;
@@ -47,10 +47,11 @@ public:
     }
     bool getAvailable()
     {
-        return available;   
+        return available;
     }
 };
-void addNewBook(vector<Book> &books) {
+void addNewBook(vector<Book> &books)
+{
     string ltitle;
     string lauthor;
     string lisbn;
@@ -59,8 +60,10 @@ void addNewBook(vector<Book> &books) {
     cin >> lisbn;
 
     // Check if book already present
-    for (int i = 0; i < books.size(); i++) {
-        if (books[i].getISBN() == lisbn) {
+    for (int i = 0; i < books.size(); i++)
+    {
+        if (books[i].getISBN() == lisbn)
+        {
             cout << "Book Already Present..." << endl;
             return;
         }
@@ -76,75 +79,82 @@ void addNewBook(vector<Book> &books) {
     cout << "Is Now Available? ";
     cin >> lavailable;
 
-    Book newBook(ltitle, lauthor, lisbn, lavailable);
+    Book newBook(ltitle, lauthor, lisbn, lavailable); // Object creation
     books.push_back(newBook);
-    cout << ltitle << " Book successfully added!!!"<<endl;
+    cout << ltitle << " Book successfully added!!!" << endl;
 }
 
-void removeBook(vector<Book> &books) {
+void removeBook(vector<Book> &books)
+{
     string isbn1;
     cout << "Enter ISBN of book to delete: ";
     cin >> isbn1;
-    for (int i = 0; i < books.size(); i++) {
-        if (books[i].getISBN() == isbn1) {
+    for (int i = 0; i < books.size(); i++)
+    {
+        if (books[i].getISBN() == isbn1)
+        {
             books.erase(books.begin() + i);
-            cout <<books[i].getTitle()<< " Book Deleted Successfully!!!" << endl;
+            cout << books[i].getTitle() << " Book Deleted Successfully!!!" << endl;
             return; // Return after deleting the book
         }
     }
     cout << "Book Not Found!!!" << endl;
 }
-void searchBook(vector<Book> &books){
+void searchBook(vector<Book> &books)
+{
     string namebook;
     cout << "Enter the name of book you want to search:";
     cin >> namebook;
-    for (int i = 0; i < books.size();i++){
-        if(books[i].getTitle()==namebook){
+    for (int i = 0; i < books.size(); i++)
+    {
+        if (books[i].getTitle() == namebook)
+        {
             cout << "Book is Available at rack no: " << i + 1;
             return;
-        }}
-    
-        cout << "Book Not Available!!!SORRY";
-        
+        }
     }
+
+    cout << "Book Not Available!!!SORRY";
+}
 
 int main()
 {
     vector<Book> books;
     books.push_back(Book("HarryPotter", "J.K.Rowling", "#00fff", true));
     char choice;
-   
-    do{
+
+    do
+    {
         system("cls");
-         int op;
-        cout << "---------------------------------"<<endl;
-        cout << "Welcome to Library Management System"<<endl;
-        cout << "---------------------------------"<<endl;
+        int op;
+        cout << "---------------------------------" << endl;
+        cout << "Welcome to Library Management System" << endl;
+        cout << "---------------------------------" << endl;
         cout << "Menu :" << endl;
-        cout << "1. Add Book"<<endl;
-        cout << "2.Remove Book"<<endl;
-        cout << "3.Search for Book"<<endl;
-        cout << "4.Exit"<<endl;
+        cout << "1. Add Book" << endl;
+        cout << "2.Remove Book" << endl;
+        cout << "3.Search for Book" << endl;
+        cout << "4.Exit" << endl;
         cout << endl;
         cout << "Enter Your Choice:";
         cin >> op;
-        switch(op){
-            case 1:
-                addNewBook(books);
-                break;
-            case 2:
-                removeBook(books);
-                break;
-            case 3:
-                searchBook(books);
-                break;
-            case 4:
-                exit(1);
-            default:
-                cout << "Invalid Number" << endl;
+        switch (op)
+        {
+        case 1:
+            addNewBook(books);
+            break;
+        case 2:
+            removeBook(books);
+            break;
+        case 3:
+            searchBook(books);
+            break;
+        case 4:
+            exit(1);
+        default:
+            cout << "Invalid Number" << endl;
         }
         cout << "Do you want to continue[Yes/No]:";
         cin >> choice;
-    } while (choice=='y' || choice=='Y');
-
+    } while (choice == 'y' || choice == 'Y');
 }
